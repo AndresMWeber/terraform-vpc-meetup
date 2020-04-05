fffea80e429c6a58806a58282c72fc9639d76477
Andres Weber <andresmweber@gmail.com>
2020-04-05 14:24:44 -0400

    Added http ingress

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -11,6 +11,13 @@ resource "aws_security_group" "terraform-vpc-meetup" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        from_port = 80
+        protocol = "tcp"
+        to_port = 80
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
         from_port = 0
         protocol = "-1"
