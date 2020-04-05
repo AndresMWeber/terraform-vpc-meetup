5832d28d6af8f9112e7a1418745541970a5d3ca6
Andres Weber <andresmweber@gmail.com>
2020-04-05 14:40:12 -0400

    Fixing bugs.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo yum update -y
-sudo yum install httpd -yum
+sudo yum install httpd -y
 sudo service httpd start
 sudo chkconfig httpd on
+sudo mkdir -p /var/www/html/
 echo "<h1>Deployed via AWS Cloud Automation</h1>" | sudo tee /var/www/html/index.html
