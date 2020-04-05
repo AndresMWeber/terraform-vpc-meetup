f34bd0e4f6541fea5b803d222326f9d124452e6a
Andres Weber <andresmweber@gmail.com>
2020-04-05 13:42:54 -0400

    Added EC2 pem key.

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,6 +1,9 @@
 resource "aws_instance" "terraform-vpc-meetup-ec2" {
     ami = "ami-0a887e401f7654935"
     instance_type = "t2.micro"
+
+    key_name = "terraform-vpc-meetup-ec2"
+
     tags = {
         Owner = "terraform-vpc-meetup"
         Project = "aws-cloud-automation"
