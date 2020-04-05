7ed57aed91039cb703da1b53733314dd17ee458a
Andres Weber <andresmweber@gmail.com>
2020-04-05 12:15:45 -0400

    Added bucket object and set read acl to public

---
+++ aws_s3_bucket_object.tf
@@ -0,0 +1,12 @@
+resource "aws_s3_bucket_object" "terraform-vpc-meetup-bucket-object" {
+    bucket = aws_s3_bucket.terraform-vpc-meetup-s3.bucket
+    key = "hello.html"
+    source = "hello.html"
+
+    acl = "public-read"
+
+    tags = {
+        Owner = "terraform-vpc-meetup"
+        Project = "terraform-vpc-meetup"
+    }
+}
